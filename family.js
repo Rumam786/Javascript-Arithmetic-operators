93ef11f2deb3fa872a7de5c8c5a07e3b53c1bf10
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-05-01 00:09:53 +0500

    Add files via upload

---
+++ family.js
@@ -0,0 +1,51 @@
+// Addition
+var x = 10;
+var v = 5;
+var c = x+v;
+document.write(c);
+document.write("<br>");
+// Subtraction
+var x = 10;
+var v = 5;
+var c = x-v;
+document.write(c);
+document.write("<br>");
+// Multiplication
+var x = 10;
+var v = 5;
+var c = x*v;
+document.write(c);
+document.write("<br>");
+// Division
+var x = 10;
+var v = 5;
+var c = x/v;
+document.write(c);
+document.write("<br>");
+// modulus/Remainder
+var x = 10;
+var v = 5;
+var c = x%v;
+document.write(c);
+document.write("<br>");
+// Exponentation
+var x = 10;
+var v = 5;
+var c = x**v;
+document.write(c);
+document.write("<br>");
+// Increment
+var x = 10;
+var v = 5;
+document.write(x+v);
+document.write("<br>");
+x++;
+document.write(x+v);
+document.write("<br>");
+// Decrement
+var x = 10;
+var v = 5;
+document.write(x+v);
+document.write("<br>");
+x--;
+document.write(x+v);
